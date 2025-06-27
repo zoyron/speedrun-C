d7b8979c5458c8287212e8d499f91e0d3f9cbccd
zoyron <sgrarora3@gmail.com>
2025-06-27 15:53:28 +0530

    update the phonebook code using struct

--- phonebook.c
+++ phonebook.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct  {
+  char *name;
+  char *numbers;
+}person;
+
 int main() {
-  char *names[] = {"Sagar", "Shubham", "Srishti"};
-  char *numbers[] = {"7838387677", "8800715420", "8506020300"};
+  person people[3]; // making an array of length 3, the datatype of the array is " struct person"
+  people[0].name = "Sagar";
+  people[0].numbers = "7838387677";
+
+  people[1].name = "Shubham";
+  people[1].numbers = "8800715420";
+
+  people[2].name = "Srishti";
+  people[2].numbers = "8506020300";
+
   char name[100];
   printf("Enter a name that you want to find: ");
   scanf("%s", name);
   for (int i = 0; i < 3; i++) {
-    if (strcmp(names[i], name) == 0) {
-      printf("Found %s\n", numbers[i]);
+    if (strcmp(people[0].name, name) == 0) {
+      printf("Found %s\n", people[i].numbers);
       return 0;
     }
   }
