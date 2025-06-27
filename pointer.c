73200379ad1ce5d0e42595f3af51c8b91ababa86
zoyron <sgrarora3@gmail.com>
2025-06-28 03:29:53 +0530

    update pointer.c

--- pointer.c
+++ pointer.c
@@ -3,7 +3,13 @@
 int main(void)
 {
     int n = 50;
-    printf("%p\n", &n);
+    int *p = &n; // p mein n ka address store hai
+
+    // p ko print karne se address milega
+    printf("Address (p ki value): %p\n", p);
+
+    // *p ko print karne se us address par rakhi value milegi
+    printf("Value at address (*p ki value): %d\n", *p);
+
     return 0;
 }
-
